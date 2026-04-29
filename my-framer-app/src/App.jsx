6b58955f5d1274bd1791582dc8e0391018f9bda5
mtt2701 <48597692+mtt2701@users.noreply.github.com>
2026-04-28 17:27:59 -0700

    motion.div and motion.button

--- my-framer-app/src/App.jsx
+++ my-framer-app/src/App.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 export default function App() {
   return (
     <div style={{ 
@@ -9,26 +11,40 @@ export default function App() {
       overflow: "hidden" 
     }}>
 
-      <div
+      <motion.div
+        drag
+        dragConstraints={{ left: -100, right: 100, top: -100, bottom: 100 }}
+        dragElastic={0.2}
+        whileTap={{ cursor: "grabbing" }}
         style={{
-            width: 180,
-            height: 180,
-            background: "linear-gradient(135deg, #38bdf8, #818cf8)",
-            borderRadius: 40,
-            cursor: "grab",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            color: "white",
-            fontWeight: "bold",
-            fontSize: "1.5rem",
-            boxShadow: "0px 10px 20px rgba(0,0,0,0.3)"
-          }}
+          width: 180,
+          height: 180,
+          background: "linear-gradient(135deg, #38bdf8, #818cf8)",
+          borderRadius: 40,
+          cursor: "grab",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+          fontWeight: "bold",
+          fontSize: "1.5rem",
+          boxShadow: "0px 10px 20px rgba(0,0,0,0.3)"
+        }}
       >
-        
-        Drag Me!
-      </div>
-
+        <motion.button
+            initial={{opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ 
+                type: "spring",
+                stiffness: 400,
+                damping: 10
+            }}
+        >
+            Click Me!
+        </motion.button>
+      </motion.div>
     </div>
   );
 }
